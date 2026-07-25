c650bc4c6b1972ed800f61ea5c631df043d9aeba
Shafan-cyber <shafanabitha691@gmail.com>
2026-07-25 12:50:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,21 +20,10 @@ const iconCode = data.weather[0].icon;
 document.querySelector(".weather-icon").src =
 `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
     
-const weatherIcon = document.querySelector(".weather-icon");
+const iconCode = data.weather[0].icon;
 
-if (data.weather[0].main === "Clouds") {
-    weatherIcon.src = "images/clouds.png";
-} else if (data.weather[0].main === "Clear") {
-    weatherIcon.src = "images/clear.png";
-} else if (data.weather[0].main === "Rain") {
-    weatherIcon.src = "images/rain.png";
-} else if (data.weather[0].main === "Drizzle") {
-    weatherIcon.src = "images/drizzle.png";
-} else if (data.weather[0].main === "Mist") {
-    weatherIcon.src = "images/mist.png";
-} else if (data.weather[0].main === "Snow") {
-    weatherIcon.src = "images/snow.png";
-}
+document.querySelector(".weather-icon").src =
+`https://openweathermap.org/img/wn/${iconCode}@2x.png`;
     
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
